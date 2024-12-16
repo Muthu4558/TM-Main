2be7081bd5197afcf3909f538ff2a93d8fb50165
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-12-16 11:38:01 +0530

    Update UserDetails.jsx

--- client/src/pages/UserDetails.jsx
+++ client/src/pages/UserDetails.jsx
@@ -24,7 +24,7 @@ const UserDetails = () => {
           setError(data.message);
         }
       } catch (err) {
-        setError("An error occurred while fetching user data 123.");
+        setError("An error occurred while fetching user data.");
       } finally {
         setLoading(false);
       }
