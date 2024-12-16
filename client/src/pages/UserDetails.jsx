0ce17b2aaa9cec6b9aa6c007dbc626ab12cccb11
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-12-16 16:25:28 +0530

    Update UserDetails.jsx

--- client/src/pages/UserDetails.jsx
+++ client/src/pages/UserDetails.jsx
@@ -15,7 +15,7 @@ const UserDetails = () => {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await fetch(`/api/users/${userId}`);
+        const response = await fetch(`/api/user/${userId}`);
         const data = await response.json();
 
         if (response.ok) {
