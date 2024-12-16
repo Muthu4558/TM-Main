f95a8780c6de73aeebde84920e18db952228feed
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-12-16 16:12:09 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -103,7 +103,7 @@ const App = () => {
           <Route path='/todo/:status' element={<Tasks />} />
           <Route path='/overdue/:status' element={<Tasks />} />
           <Route path='/team' element={<Users />} />
-          <Route path="/users/:userId/tasks" element={<UserDetails />} />
+          <Route path="/user/:userId/tasks" element={<UserDetails />} />
           <Route path='/trashed' element={<Trash />} />
           <Route path='/task/:id' element={<TaskDetails />} />
         </Route>
