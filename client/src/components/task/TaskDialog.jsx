07268c93de07b1cff21e1dac75b723f0505b229c
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-12-20 09:42:48 +0530

    Update TaskDialog.jsx

--- client/src/components/task/TaskDialog.jsx
+++ client/src/components/task/TaskDialog.jsx
@@ -116,16 +116,6 @@ const TaskDialog = ({ task }) => {
           <Menu.Button className='inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-gray-600 '>
             <BsThreeDots />
           </Menu.Button>
-
-          <Transition
-            as={Fragment}
-            enter='transition ease-out duration-100'
-            enterFrom='transform opacity-0 scale-95'
-            enterTo='transform opacity-100 scale-100'
-            leave='transition ease-in duration-75'
-            leaveFrom='transform opacity-100 scale-100'
-            leaveTo='transform opacity-0 scale-95'
-          >
             <Menu.Items className='absolute p-4 right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
               <div className='px-1 py-1 space-y-2'>
                 {items.map((el) => (
@@ -162,7 +152,6 @@ const TaskDialog = ({ task }) => {
                 </Menu.Item>
               </div>
             </Menu.Items>
-          </Transition>
         </Menu>
       </div>
 
