89ec23ae975e7df42459c693f804f462e843f41f
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-12-30 11:31:29 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { setOpenSidebar } from "../redux/slices/authSlice";
 import clsx from "clsx";
-import taskgo from "../assets/images/taskgo.png"
+import newlogo1 from "../assets/images/newlogo1.png"
 
 const linkData = [
   {
@@ -96,7 +96,7 @@ const Sidebar = () => {
   return (
     <div className='w-ful h-full flex flex-col gap-6 p-5'>
 
-      <img className="w-48" src={taskgo} alt="Nizcare-Logo" />
+      <img className="w-67 mt-1" src={newlogo1} alt="Nizcare-Logo" />
 
       <div className='flex-1 flex flex-col gap-y-5 py-8'>
         {sidebarLinks.map((link) => (
